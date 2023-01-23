215bd97f36ff1e894ea2bb87fd7dda74b759ccdd
Sumit <sumit@kodnest.com>
2023-01-23 23:04:57 +0530

    mailer added

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,20 +1,22 @@
 "use strict";
 const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
 async function sendMail({ to, subject, template, message }) {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: "sb78639@gmail.com", // generated ethereal user
-      pass: "<Password comes here>", // generated ethereal password
+      user: process.env.GOOGLE_USERNAME, // generated ethereal user
+      pass: process.env.GOOGLE_APP_PASSWORD, // generated ethereal password
     },
   });
 
   try {
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: "sb78639@gmail.com", // sender address
+      from: process.env.GOOGLE_USERNAME, // sender address
       to: to, // list of receivers
       subject: subject, // Subject line
       text: message, // plain text body
